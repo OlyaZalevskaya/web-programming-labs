da03aeb34add640ef0a0b4dc37d2eedc225d06e1
Ольга Залевская <olga.zalevskaya03@mail.ru>
2023-06-05 18:29:08 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -11,3 +11,11 @@ function showDate() {
     out.style.fontFamily = 'Monserat'
     out.style.fontSize = '20px'
 }
+function showDaysCount(){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    document.getElementById('rojd').innerHTML = "Количество дней с даты рождения: " + daysCount;
+}
